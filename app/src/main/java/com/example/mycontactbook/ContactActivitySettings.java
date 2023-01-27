55e7c7b3a571b6ae7f3345c69cbd6a97f54ed1f4
jmaude <janeymaude@gmail.com>
2023-01-26 20:34:37 -0500

    Nav Bar & initSettings()

--- app/src/main/java/com/example/mycontactbook/ContactActivitySettings.java
+++ app/src/main/java/com/example/mycontactbook/ContactActivitySettings.java
@@ -2,7 +2,12 @@ package com.example.mycontactbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
 
 public class ContactActivitySettings extends AppCompatActivity {
 
@@ -10,5 +15,67 @@ public class ContactActivitySettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_settings);
+        initListButton();
+        initMapButton();
+        initSettingsButton();
+    }
+    private void initListButton() {
+        ImageButton ibList = findViewById(R.id.imageButtonList);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view)  {
+                Intent intent = new Intent(ContactActivitySettings.this, ContactListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initMapButton() {
+        ImageButton ibList = findViewById(R.id.imageButtonMap);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view)  {
+                Intent intent = new Intent(ContactActivitySettings.this, ContactMapActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initSettingsButton() {
+        ImageButton ibSettings = findViewById(R.id.imageButtonSettings);
+        ibSettings.setEnabled(false);
+        //disables button
+    }
+
+    private void initSettings(){
+        String sortBy = getSharedPreferences("MyContactListPreferences",
+                Context.MODE_PRIVATE).getString("sortfield", "contactname");
+        String sortOrder = getSharedPreferences("MyContactListPreferences",
+                Context.MODE_PRIVATE).getString("sortorder", "ASC");
+        /*string var declared - value for the field to sort contacts retrieved from SharedPref
+        SharedPref method gets SharedPref obj - no need for multiple SP objects
+        */
+        RadioButton rbName = findViewById(R.id.radioName);
+        RadioButton rbCity = findViewById(R.id.radioCity);
+        RadioButton rbBirthDay = findViewById(R.id.radioBirthday);
+        if (sortBy.equalsIgnoreCase("contactname")){
+            rbName.setChecked(true);
+        }
+        else if (sortBy.equalsIgnoreCase("city")){
+            rbCity.setChecked(true);
+        }
+        else {
+            rbBirthDay.setChecked(true);
+            //automatically sorts by birthday
+        }
+
+        RadioButton rbAscending = findViewById(R.id.radioAscending);
+        RadioButton rbDescending = findViewById(R.id.radioDescending);
+        if (sortOrder.equalsIgnoreCase("ASC")){
+            rbAscending.setChecked(true);
+        }
+        else {
+            rbDescending.setChecked(true);
+        }
     }
 }
