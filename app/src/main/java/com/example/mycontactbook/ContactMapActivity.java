22cbf6870facbdd7d277ba1b661a814ecb60a2cf
jmaude <janeymaude@gmail.com>
2023-02-12 21:39:14 -0500

    7.6 I know you right

--- app/src/main/java/com/example/mycontactbook/ContactMapActivity.java
+++ app/src/main/java/com/example/mycontactbook/ContactMapActivity.java
@@ -14,7 +14,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
+import com.google.android.material.snackbar.Snackbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
