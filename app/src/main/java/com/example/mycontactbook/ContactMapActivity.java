8b829c86e964840fec06b98d78763b940419803d
jmaude <janeymaude@gmail.com>
2023-02-15 12:08:51 -0500

    7.12

--- app/src/main/java/com/example/mycontactbook/ContactMapActivity.java
+++ app/src/main/java/com/example/mycontactbook/ContactMapActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -14,33 +15,60 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.material.snackbar.Snackbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.location.*;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
 import java.io.IOException;
 import java.util.List;
 
-public class ContactMapActivity extends AppCompatActivity {
-
-    LocationManager locationManager;
-    LocationListener gpsListener;
-    LocationListener networkListener;
-    Location currentBestLocation;
+public class ContactMapActivity extends AppCompatActivity implements
+        OnMapReadyCallback {
+    //OMRC- interface used to notify the activity that the map has been downloaded and is ready to be used
 
     final int PERMISSION_REQUEST_LOCATION = 101;
+    GoogleMap gMap;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationRequest locationRequest;
+    LocationCallback locationCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_map);
-        initGetLocationButton();
-    }
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        SupportMapFragment mapFragment = (SupportMapFragment)
+            getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this); //map is retrieved asyn - onMapReady()
+
+        createLocationRequest(); //locationlistener
+        createLocationCallback(); //process location changes
+
+        initListButton();
+        initMapButton();
+        initSettingsButton();
+
+
+    }
     @Override
     public void onPause() {
         super.onPause();
@@ -54,122 +82,113 @@ public class ContactMapActivity extends AppCompatActivity {
                         PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        try {
-            locationManager.removeUpdates(gpsListener);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
-    private void initGetLocationButton () {
-        Button locationButton = (Button) findViewById(R.id.buttonGetLocation);
-        locationButton.setOnClickListener(new View.OnClickListener() {
 
+
+    private void createLocationRequest(){
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        //create param of location request-sets standard monitoring interval,
+        //fastest int needed, accuracy
+    }
+    //method responds to changes prov by FLPC- if location results
+    // - loops through results to display as Toast
+    private void createLocationCallback(){
+        locationCallback = new LocationCallback(){
             @Override
-            public void onClick(View view) {
-
-                try {
-                    if (Build.VERSION.SDK_INT >= 23) {
-                        if (ContextCompat.checkSelfPermission(ContactMapActivity.this,
-                                Manifest.permission.ACCESS_FINE_LOCATION) !=
-                                PackageManager.PERMISSION_GRANTED) {
-                            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                                    ContactMapActivity.this,
-                                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-                                Snackbar.make(findViewById(R.id.activity_contact_map),
-                                                "MyContactList requires this permission to locate " +
-                                                        "your contacts", Snackbar.LENGTH_INDEFINITE)
-                                        .setAction("OK", new View.OnClickListener() {
-                                            @Override
-                                            public void onClick(View view) {
-                                                ActivityCompat.requestPermissions(
-                                                        ContactMapActivity.this,
-                                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                                                        PERMISSION_REQUEST_LOCATION);
-                                            }
-                                        })
-                                        .show();
-                            } else {
-                                ActivityCompat.requestPermissions(ContactMapActivity.this,
-                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                                        PERMISSION_REQUEST_LOCATION);
-                            }
-                        } else {
-                            startLocationUpdates();
-                        }
-                    } else {
-                        startLocationUpdates();
-                    }
-                } catch (Exception e) {
-                    Toast.makeText(getBaseContext(), "Error requesting prmission",
-                            Toast.LENGTH_LONG).show();
+            public void onLocationResult (LocationResult locationResult){
+                if (locationResult == null){
+                    return;
                 }
-
+                for (Location location : locationResult.getLocations()){
+                    Toast.makeText(getBaseContext(), "Lat: " + location.getLatitude() +
+                            "Long: " + location.getLongitude() +
+                            "Accuracy: " + location.getAccuracy(), Toast.LENGTH_LONG).show();
+                }
             }
-        });
+        };
     }
-    private void startLocationUpdates() {
+    //starts location updates - checks permissions
+    private void startLocationUpdates(){
+        if(Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission(getBaseContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(getBaseContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                     PackageManager.PERMISSION_GRANTED)
+        {
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest,
+                locationCallback, null);
+        gMap.setMyLocationEnabled(true);
+    }
+    //stops location updates from FLPC
+    private void stopLocationUpdates(){
+        if(Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission(getBaseContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION) !=
+                            PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(getBaseContext(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED)
+        {
+            return;
+        }
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+    }
 
+    //recieves google map obj and assigns it to gMap var - map type set
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        gMap= googleMap;
+        gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL); //options- MAP_TYPE_SATELLITE, MAP_TYPE_TERRAIN
         try {
-            locationManager = (LocationManager) getBaseContext().getSystemService(Context.LOCATION_SERVICE);
-            gpsListener = new LocationListener() {
-                /*getSystemService method is sent to the activity's context with a parameter that tells the
-                context that you want the location service manager.
-                getBaseContext is used to get the root context- this case activity
-                 */
-                @Override
-                public void onLocationChanged(@NonNull Location location) {
-                    TextView txtLatitude = (TextView) findViewById(R.id.textLattitude);
-                    TextView txtLongitude = (TextView) findViewById(R.id.textLongitude);
-                    TextView txtAccuracy = (TextView) findViewById(R.id.textAccuracy);
-                    txtLongitude.setText(String.valueOf(location.getLatitude()));
-                    txtLatitude.setText(String.valueOf(location.getLongitude()));
-                    txtAccuracy.setText(String.valueOf(location.getAccuracy()));
-                    //when a location is detected it is reported to this method as a location obj
+            if (Build.VERSION.SDK_INT >= 23) {
+                if (ContextCompat.checkSelfPermission(ContactMapActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(
+                            ContactMapActivity.this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                        Snackbar.make(findViewById(R.id.activity_contact_map),
+                                        "MyContactList requires this permission to locate " +
+                                                "your contacts", Snackbar.LENGTH_INDEFINITE)
+                                .setAction("OK", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        ActivityCompat.requestPermissions(
+                                                ContactMapActivity.this,
+                                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                                PERMISSION_REQUEST_LOCATION);
+                                    }
+                                })
+                                .show();
+                    } else {
+                        ActivityCompat.requestPermissions(ContactMapActivity.this,
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                PERMISSION_REQUEST_LOCATION);
+                    }
+                } else {
+                    startLocationUpdates();
                 }
-
-                public void onStatusChanged(String provider, int status, Bundle extras) {}
-                public void onProviderEnabled(String provider) {}
-                public void onProviderDisabled(String provider) {}
-                //required by by location listener + onLocationChanged
-            };networkListener = new LocationListener() {
-                /*getSystemService method is sent to the activity's context with a parameter that tells the
-                context that you want the location service manager.
-                getBaseContext is used to get the root context- this case activity
-                 */
-                @Override
-                public void onLocationChanged(@NonNull Location location) {
-                    TextView txtLatitude = (TextView) findViewById(R.id.textLattitude);
-                    TextView txtLongitude = (TextView) findViewById(R.id.textLongitude);
-                    TextView txtAccuracy = (TextView) findViewById(R.id.textAccuracy);
-                    txtLongitude.setText(String.valueOf(location.getLatitude()));
-                    txtLatitude.setText(String.valueOf(location.getLongitude()));
-                    txtAccuracy.setText(String.valueOf(location.getAccuracy()));
-                    //when a location is detected it is reported to this method as a location obj
-                }
-
-                public void onStatusChanged(String provider, int status, Bundle extras) {}
-                public void onProviderEnabled(String provider) {}
-                public void onProviderDisabled(String provider) {}
-                //required by by location listener + onLocationChanged
-            };
-
-            locationManager.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER, 0, 0, gpsListener);
-                   /*LM is sent message requestLocationUpdates to begin listening for location changes
-                   with non min time between updates and changes
-                    */
-            locationManager.requestLocationUpdates(
-                    LocationManager.NETWORK_PROVIDER, 0, 0, gpsListener);
-                   /*LM is sent message requestLocationUpdates to begin listening for location changes
-                   with non min time between updates and changes
-                    */
+            } else {
+                startLocationUpdates();
+            }
         } catch (Exception e) {
-            Toast.makeText(getBaseContext(), "Error, Location not available",
+            Toast.makeText(getBaseContext(), "Error requesting prmission",
                     Toast.LENGTH_LONG).show();
         }
     }
+
+
+
+
     @Override
     //request response sends request code - determines which permission request
     public void onRequestPermissionsResult (int requestCode,
@@ -191,17 +210,33 @@ public class ContactMapActivity extends AppCompatActivity {
         }
     }
 
-    private boolean isBetterLocation(Location location){
-        boolean isBetter = false;
-        if (currentBestLocation == null){
-            isBetter = true;
-        }
-        else if (location.getAccuracy() <= currentBestLocation.getAccuracy()){
-            isBetter = true;
-        }
-        else if (location.getTime() - currentBestLocation.getTime() > 5*60*1000){
-            isBetter = true;
-        }
-        return isBetter;
+
+    private void initListButton() {
+        ImageButton ibList = findViewById(R.id.imageButtonList);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view)  {
+                Intent intent = new Intent(ContactMapActivity.this, ContactListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
+
+    private void initMapButton() {
+        ImageButton ibList = findViewById(R.id.imageButtonMap);
+        ibList.setEnabled(false);
+            }
+
+
+    private void initSettingsButton() {
+        ImageButton ibSettings = findViewById(R.id.imageButtonSettings);
+        ibSettings.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(ContactMapActivity.this, ContactActivitySettings.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
